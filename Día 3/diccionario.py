e811a993ea931033659d593dd631964435f3d115
miguel <gambito946@gmail.com>
2025-08-19 22:22:54 -0400

    Término lección de diccinarios, e inicio de lección de tuplas

--- Día 3/diccionario.py
+++ Día 3/diccionario.py
@@ -18,7 +18,29 @@ print("-----------------------------------------------------")
 
 dic2 = {"c1":['a','b','c'], "c2":['d','e','f']}
 print(dic2["c2"][1].upper())
-print("---------------------------------------------------------------")
+print("------------------------------------------------------")
 
+dic3 = {1:"A", 2:"B",3:"C",}
+print(dic3)
 
+dic3[4] = "D"
+print(dic3)
+
+dic3[2] = 'B'
+print(dic3)
+
+print(dic3.keys())
+print(dic3.values())
+print(dic3.items())
+print("--------------------------------------------------")
+
+mi_dict = {"valores_1":{"v1":3,"v2":6},"puntos":{"points1":9,"points2":[10,300,15]}}
+print(mi_dict["puntos"]["points2"][1])
+print("---------------------------------------------------------")
+
+mi_dic = {"nombre":"Karen", "apellido":"Jurgens", "edad":35, "ocupacion":"Periodista"}
+mi_dic["edad"] = 36
+mi_dic["ocupacion"] = "Editora"
+mi_dic["pais"] = "Colombia"
+print(mi_dic)
 
---
+++ Día 3/tuplas.py
@@ -0,0 +1,16 @@
+my_tuple = (1,2,(50,100,150),4,5)
+my_tuple = list(my_tuple)
+my_tuple = tuple(my_tuple)
+print(type(my_tuple))
+
+print('--------------------------------------------')
+
+t = (1,2,3)
+
+x,y,z = t
+print(x,y,z)
+
+list = [1,10,100]
+
+a,b,c = list
+print(a,b,c)
